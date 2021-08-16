b73f0967ef2f572f364abef7f7d724ed5d2e76da
Jeremy <jeremyrchow@gmail.com>
2021-08-16 00:31:57 -0700

    Center feed section completed

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,26 @@ import React from 'react';
 import "./Feed.css";
 import StoryReel from './StoryReel';
 import Poster from './Poster';
+import Post from './Post';
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <Poster />
-      
+      <Post
+        profilePic = "https://media-exp1.licdn.com/dms/image/C5603AQFAKN4ErXYy1w/profile-displayphoto-shrink_200_200/0/1554765001760?e=1634774400&v=beta&t=ZI3wZE0CAV0IrQCiYhelSZd8oHFKiLqqG8u6O4eas0U"
+        message="Hello from californiaHello from californiaHello from californiaHello from california"
+        timestamp="12:09 AM"
+        username="jchowster"
+        image="https://content.fortune.com/wp-content/uploads/2019/10/GettyImages-1158402857.jpg?resize=750,500" 
+         />
+      <Post
+        profilePic = "https://media-exp1.licdn.com/dms/image/C5603AQFAKN4ErXYy1w/profile-displayphoto-shrink_200_200/0/1554765001760?e=1634774400&v=beta&t=ZI3wZE0CAV0IrQCiYhelSZd8oHFKiLqqG8u6O4eas0U"
+        message= "Hello from not california"
+        timestamp="12:09 AM"
+        username="jchowster"
+         />
+      <Post />
     </div>
   )
 }
--- src/Poster.js
+++ src/Poster.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Poster.css'
+
+import {Avatar} from "@material-ui/core";
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryicon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+
 function Poster() {
+  const [input,setInput] = useState("")
+  const [imageUrl, setImageUrl] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+    setImageUrl("");
+  };
   return (
     <div className="poster">
-      
+      <div className="poster__top">
+        <Avatar />
+        <form>
+          <input 
+            className="poster__input" 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder={"What's on your mind?"} 
+          />
+          <input
+            value={imageUrl}
+            onChange ={(e) => setImageUrl(e.target.value)}
+            placeholder="image URL (optional)"/>
+
+          <button onClick={handleSubmit} type="submit">
+            Hidden submit text
+          </button>
+        </form>
+      </div>
+      <div className="poster__bottom">
+        <div className="poster__option">
+          <VideocamIcon style= {{color: "red"}}/>
+          <h3> Live Video </h3>
+        </div>
+        <div className="poster__option">
+          <PhotoLibraryicon style= {{color: "blue"}}/>
+          <h3> Live Video </h3>
+        </div>
+        <div className="poster__option">
+          <InsertEmoticonIcon style= {{color: "orange"}}/>
+          <h3> Live Video </h3>
+        </div>
+        
+      </div>
     </div>
   )
 }
