cee3f0c897e7c95a0c656a24f8848c4e1fa97452
Jeremy <jeremyrchow@gmail.com>
2021-08-16 12:38:02 -0700

    Added google auth login

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDRCha4wPV9LfJfVEqw4OrNKNGW6serWLw",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:1050297187868:web:95aabbecc834918cab29d9",
     measurementId: "G-MC2MCGNGE2"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
