3f808d1e61c0697556905ce6197bacb92235af62
Jeremy <jeremyrchow@gmail.com>
2021-08-15 22:14:16 -0700

    First commit

---
+++ src/Header.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./Header.css";
+
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from"@material-ui/icons/Add";
+import { IconButton } from '@material-ui/core';
+import NotificationsNoneTwoToneIcon from '@material-ui/icons/NotificationsNoneTwoTone';
+import CommentTwoToneIcon from '@material-ui/icons/CommentTwoTone';
+import ExpandMoreTwoToneIcon from '@material-ui/icons/ExpandMoreTwoTone';
+function Header() {
+return (
+  <div className="header">
+    <div className="header__left">
+      <img src="../logo192.png"></img>
+    </div>
+    <div className="header__input">
+      <SearchIcon />
+      <input type="text"/>
+    </div>
+
+    <div className="header__center">
+      <div className="header__option">
+        <HomeIcon fontsize="large" />
+      </div>
+      <div className="header__option">
+        <VideoLibraryIcon fontsize="large" />
+      </div>
+      <div className="header__option">
+        <PeopleIcon fontsize="large" />
+      </div>
+    </div>
+    <div className="header__right">
+      <div className="header__info">
+        <PersonIcon />
+        <h4> Jeremy Chow </h4>
+      </div>
+      <IconButton>
+        <AddIcon/>
+      </IconButton>
+      <IconButton>
+        <NotificationsNoneTwoToneIcon/>
+      </IconButton>
+      <IconButton>
+        <CommentTwoToneIcon/>
+      </IconButton>
+      <IconButton>
+        <ExpandMoreTwoToneIcon />
+      </IconButton>
+
+    </div>
+    
+  </div>
+)
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDRCha4wPV9LfJfVEqw4OrNKNGW6serWLw",
+    authDomain: "facebook-clone-c8e80.firebaseapp.com",
+    projectId: "facebook-clone-c8e80",
+    storageBucket: "facebook-clone-c8e80.appspot.com",
+    messagingSenderId: "1050297187868",
+    appId: "1:1050297187868:web:95aabbecc834918cab29d9",
+    measurementId: "G-MC2MCGNGE2"
+  };
